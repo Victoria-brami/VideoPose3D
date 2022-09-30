c9beed087cbe1bcde32cd3b67a89f7ccb170cec2
Victoria-brami <victoria.brami88@gmail.com>
2022-09-30 10:53:19 +0200

    create rendering folder

---
+++ rendering/config/default.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3  Line 1
+# -*- coding: utf-8 -*- Line 2
+#----------------------------------------------------------------------------
+# Created By  : Victoria BRAMI   Line 3
+# Created Date: 2022/07/time ..etc
+# version ='1.0'
+# ---------------------------------------------------------------------------
+''' File to configure parameters for video rendering'''  #Line 4
+# ---------------------------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# ---------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+   print(0)
